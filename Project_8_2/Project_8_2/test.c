7b9a52d8002433653872f926cc728d6d490ae2dd
KCTLS <2279169022@qq.com>
2021-08-02 16:44:58 +0800

    初始指针（1）

---
+++ Project_8_2/Project_8_2/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* pa = &a;
+//	char* pc = &a;
+//	printf("%p\n", pa);
+//	printf("%p\n", pa + 1);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc+1);
+//	return 0;
+//}
+int main()
+{
+	int arr[10] = { 0 };
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		*(p + i) = 1;
+	}
+	return 0;
+}
