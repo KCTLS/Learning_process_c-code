ca752505c3ccd273484fb4b4cb5a381519f4f2d7
KCTLS <2279169022@qq.com>
2021-08-10 21:04:37 +0800

    数据的存储（1）

---
+++ Project_8_10/Project_8_10/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//设计一个小程序，判断当前机器的字节序
+//int check_sys(void)
+//{
+//	int a = 1;
+//	//返回1，小端
+//	//返回0，大端
+//	return *((char*)&a);
+//}
+//int main()
+//{
+//	int ret=check_sys();
+//	if (ret == 1)
+//		printf("小端\n");
+//	else
+//		printf("大端\n");
+//	return 0;
+////}
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d", a, b, c);//整形提升，有符号，无符号
+//	return 0;
+//}
