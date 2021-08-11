f738cbef18f1d02455543d3fd9cd040310459169
KCTLS <2279169022@qq.com>
2021-08-11 22:03:22 +0800

    数据的存储（2）

--- Project_8_11/Project_8_11/test.c
+++ Project_8_11/Project_8_11/test.c
@@ -19,3 +19,40 @@
 //	printf("%d\n", p->age);
 //	return 0;
 //}
+//int main()
+//{
+//	int a = -20;
+//	int b = 10;
+//	printf("%d\n", a + b);//按照补码的形式进行计算，最后格式化成为有符号的整数
+//	return 0;
+//}
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+//   
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n", strlen(a));
+//	return 0;
+//}
+unsigned char i = 0;
+int main()
+{
+	for (i = 0; i <=255; i++)
+	{ 
+		printf("hello world\n");
+	}
+	return 0;
+}
