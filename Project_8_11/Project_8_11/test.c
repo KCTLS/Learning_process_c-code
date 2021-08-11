fbf72d1547e8cebbf30d60433a46dd24b65511f9
KCTLS <2279169022@qq.com>
2021-08-11 20:58:04 +0800

    数据的存储（2）

---
+++ Project_8_11/Project_8_11/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	char a = -128;//000000000010000000
+//	printf("%u\n", a);//%u -无符号的十进制数字
+//	return 0;
+//}
+//typedef struct Stu
+//{
+//	char name[10];
+//	int age;
+//}Stu;
+//int main()
+//{
+//	Stu s1 = { "张三",  10};
+//	Stu* p =NULL;
+//	p = &s1;
+//	printf("%d\n", p->age);
+//	return 0;
+//}
