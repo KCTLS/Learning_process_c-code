da9c29faa470ba3d517e2a51d9b73f986b14f345
KCTLS <2279169022@qq.com>
2021-09-05 21:40:25 +0800

    指针笔试面试题讲解（4）

---
+++ Project_9_5/Project_9_5/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	char *c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+	char** cp[] = { c + 3, c + 2, c + 1, c };
+	char*** cpp = cp;
+
+	printf("%s\n", **++cpp);
+	printf("%s\n", *--*++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+
+	return 0;
+}
